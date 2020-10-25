36381952c001b0fb3d328413aa60e40979ec119a
warrenanson <warren45612378@gmail.com>
2020-10-25 16:48:00 +0800

    first commit

---
+++ demo_file.py
@@ -0,0 +1,34 @@
+import numpy
+import nltk
+from sys import stdin
+import sys
+from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+#nltk.downloader.download('vader_lexicon')
+
+sia = SIA()
+results = []
+abuse = False
+
+
+print("Demo start!")
+
+
+with open(sys.argv[1],'r') as f:
+    for line in f.readlines():
+
+        pol_score = sia.polarity_scores(line)
+        pol_score['Word'] = line
+        results.append(pol_score)
+        print(pol_score)
+
+        if pol_score['neg'] > 0.37:
+            abuse = True
+
+        #if(pol_score['neg']>0.37):
+        #    print('[Warning] Your words are offensive! Stop abusing!\n')
+
+if abuse:
+    print("[Warning] You have been detected game abusing and will be banned from playing games for a week as punishment! ")
+
+else:
+    print("[Message] Abusing not detected!")    
